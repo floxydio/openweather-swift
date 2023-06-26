78f832b6caa0f3501a876c7bda2bd76090ee3223
floxydio <diooktar@gmail.com>
2023-06-26 23:21:43 +0700

    Initial Commit

---
+++ weatherapp/weatherappApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherappApp.swift
+//  weatherapp
+//
+//  Created by Dio Rovelino on 26/06/23.
+//
+
+import SwiftUI
+
+@main
+struct weatherappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
